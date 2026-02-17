36f4dd9efa659e7bc139d4b6fc17ed66c139312a
Unity <Unity@rbiu.local>
2026-02-17 15:14:51 +0500

    4

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/reports', [ReportController::class, 'index'])->name('report.index');
+
+Route::get('/reports/create', function () {
+    return view('report.create');
+})->name('reports.create');
